d812e93bac546054ad1ac76861c603468bfd01fb
Jasjeev <jasjeev99@gmail.com>
2024-08-08 16:36:04 +0530

    Prisma Setup Completed

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { ApolloServer } from '@apollo/server'
 import { expressMiddleware } from '@apollo/server/express4'
+import { prismaClient } from './lib/db';
 
 async function init() {
     const app = express();
@@ -17,16 +18,32 @@ async function init() {
             }
 
             type Mutation {
-                createUser(email)
+                createUser(firstName: String!,lastName: String!,email: String!,password: String!): Boolean
             }
         `,
         resolvers: {
             Query: {
                 hello: () => `Hey there I am graphql Server`,
-                say: (_,{name}:{name:String}) => `Hey ${name}`,
-            }
+                say: (_, { name }: { name: String }) => `Hey ${name}`,
+            },
+            Mutation: {
+                createUser: async (_,
+                    { firstName, lastName, email, password }:
+                        { firstName: string; lastName: string; email: string; password: string; }) => {
+                    await prismaClient.user.create({
+                        data: {
+                            firstName,
+                            lastName,
+                            email,
+                            password,
+                            salt: "random_salt"
+                        }
+                    });
+                    return true;
+                }
+            },
         }
-    }); 
+    });
 
     await gqlServer.start();
 
@@ -34,9 +51,9 @@ async function init() {
         res.json({ message: "Server is up & running" });
     })
 
-    app.use('/graphql',expressMiddleware(gqlServer))
+    app.use('/graphql', expressMiddleware(gqlServer))
 
     app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
-} 
+}
 
 init();
